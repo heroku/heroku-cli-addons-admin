25da200c21a49e86402996f4111f31152b667cce
Aman Ibrahim <amanmibra@gmail.com>
2018-06-11 16:25:36 -0700

    Added pull functionality

--- src/commands/addons/admin/manifest/pull.ts
+++ src/commands/addons/admin/manifest/pull.ts
@@ -1,25 +1,56 @@
-import {Command, flags} from '@oclif/command'
+// CommandExtension
+import CommandExtension from '../../../../CommandExtension';
 
-export default class AddonsAdminManifestPull extends Command {
-  static description = 'describe the command here'
+// heroku-cli
+import {Command, flags} from '@heroku-cli/command';
+import * as Heroku from '@heroku-cli/schema';
+import color from '@heroku-cli/color';
+
+// other packages
+import cli from 'cli-ux';
+import { writeFileSync } from 'fs';
+
+export default class Pull extends CommandExtension {
+  static description = 'fetch a manifest for a given slug';
+
+  static examples = [ `$ heroku addons:admin:fetch slowdb
+{...`, ];
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'slug'}];
 
   async run() {
-    const {args, flags} = this.parse(AddonsAdminManifestPull)
+    const {args, flags} = this.parse(Pull);
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/aman.ibrahim/Desktop/heroku-cli-addons-admin/src/commands/addons/admin/manifest/pull.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    // getting Heroku user data
+    let {body: account} = await this.heroku.get<Heroku.Account>('/account', {retryAuth: false});
+    let email = account.email;
+
+    // headers and data to sent addons API via http request
+    let defaultOptions = {
+      headers: {
+        authorization: `Basic ${Buffer.from(email + ':' + this.heroku.auth).toString('base64')}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'User-Agent': 'kensa future'
+      }
+    };
+    const slug = args.slug;
+    const host = process.env.HEROKU_ADDONS_HOST || 'https://addons.heroku.com';
+
+    // GET request
+    cli.action.start(`Fetching add-on manifest for ${color.addon(slug)}`);
+
+    const {body} = await this.heroku.get<any>(`${host}/provider/addons/${slug}`, defaultOptions);
+    console.log(color.bold(JSON.stringify(body, null, 1)));
+    cli.action.stop();
+
+    // writing addon_manifest.json
+    cli.action.start(`Updating ${color.blue('addon_manifest.json')}`);
+    writeFileSync('addon_manifest.json', JSON.stringify(body, null, 2));
+    cli.action.stop();
   }
 }
