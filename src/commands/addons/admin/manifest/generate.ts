53871f548183f3758cd3d20a33a721f98cb47971
Aman Ibrahim <amanmibra@gmail.com>
2018-06-11 16:25:51 -0700

    Formatted and added internal documentation

--- src/commands/addons/admin/manifest/generate.ts
+++ src/commands/addons/admin/manifest/generate.ts
@@ -1,22 +1,23 @@
 // CommandExtension
-import CommandExtension from '../../../../CommandExtension'
+import CommandExtension from '../../../../CommandExtension';
+
 // heroku-cli
-import {Command, flags} from '@heroku-cli/command'
-import * as Heroku from '@heroku-cli/schema'
-import color from '@heroku-cli/color'
+import {Command, flags} from '@heroku-cli/command';
+import * as Heroku from '@heroku-cli/schema';
+import color from '@heroku-cli/color';
 
 // other packages
-import cli from 'cli-ux'
-import { writeFile } from 'fs'
+import cli from 'cli-ux';
+import { writeFile } from 'fs';
 
 // utils
-import generateManifest from '../../../../utils/manifest'
+import generateManifest from '../../../../utils/manifest';
 
 export default class Generate extends CommandExtension {
-  static description = 'generate a manifest template'
+  static description = 'generate a manifest template';
 
   static examples = [ `$ oclif-example addons:admin:generate
-The file has been saved!`, ]
+The file has been saved!`, ];
 
   async run() {
     const {body: account} = await this.heroku.get<Heroku.Account>('/account', {retryAuth: false});
@@ -26,11 +27,11 @@ The file has been saved!`, ]
       this.error(color.red('Please login with Heroku credentials using `heroku login`.'));
     }
     const manifest = generateManifest({}); // this function takes in an object see utils/manifest
-    const manifestObj = JSON.stringify(manifest, null, 2)
-    cli.action.start('Generating addon_manifest')
+    const manifestObj = JSON.stringify(manifest, null, 2);
+    cli.action.start('Generating addon_manifest');
     writeFile('addon_manifest.json', manifestObj , (err) => {
       // console.log('Generating addon_manifest.json...')
-      cli.action.stop()
+      cli.action.stop();
       if (err) {
         console.log('The file has not been saved: \n', err);
         return;
--- src/commands/addons/admin/manifest/push.ts
+++ src/commands/addons/admin/manifest/push.ts
@@ -1,38 +1,41 @@
 // CommandExtension
-import CommandExtension from '../../../../CommandExtension'
+import CommandExtension from '../../../../CommandExtension';
 
 // heroku-cli
-import {Command, flags} from '@heroku-cli/command'
-import * as Heroku from '@heroku-cli/schema'
-import color from '@heroku-cli/color'
+import {Command, flags} from '@heroku-cli/command';
+import * as Heroku from '@heroku-cli/schema';
+import color from '@heroku-cli/color';
 
 // other packages
-import cli from 'cli-ux'
-import * as fs from 'fs';
+import cli from 'cli-ux';
+import { readFileSync, writeFileSync } from 'fs';
 
 export default class Push extends CommandExtension {
-  static description = 'push created manifest'
+  static description = 'push created manifest';
 
   static flags = {
     help: flags.help({char: 'h'}),
-  }
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'file'}];
 
   async run() {
-    const {args, flags} = this.parse(Push)
+    const {args, flags} = this.parse(Push);
 
-    let {body: account} = await this.heroku.get<Heroku.Account>('/account', {retryAuth: false})
-    let email = account.email
+    // getting Heroku user data
+    let {body: account} = await this.heroku.get<Heroku.Account>('/account', {retryAuth: false});
+    let email = account.email;
 
-    const host = process.env.HEROKU_ADDONS_HOST || 'https://addons.heroku.com'
+    const host = process.env.HEROKU_ADDONS_HOST || 'https://addons.heroku.com';
 
-    const manifest = fs.readFileSync('addon_manifest.json', 'utf8')
+    // grabbing manifest data
+    const manifest: string = readFileSync('addon_manifest.json', 'utf8');
     if (!manifest) {
-      this.error('No manifest found. Please generate a manifest before pushing.')
+      this.error('No manifest found. Please generate a manifest before pushing.');
     }
 
-    let defaultOptions = {
+    // headers and data to sent addons API via http request
+    let defaultOptions: object = {
       headers: {
         authorization: `Basic ${Buffer.from(email + ':' + this.heroku.auth).toString('base64')}`,
         'Content-Type': 'application/json',
@@ -40,16 +43,17 @@ export default class Push extends CommandExtension {
         'User-Agent': 'kensa future'
       },
       body: JSON.parse(manifest)
-    }
+    };
 
-    cli.action.start(`Pushing manifest`)
-
-    const {body} = await this.heroku.post<any>(`${host}/provider/addons`, defaultOptions)
-    console.log(body)
+    // POST request
+    cli.action.start(`Pushing manifest`);
+    const {body} = await this.heroku.post<any>(`${host}/provider/addons`, defaultOptions);
+    console.log(body);
     cli.action.stop();
 
-    cli.action.start('Updating addon_manifest.json')
-    fs.writeFileSync('addon_manifest.json', JSON.stringify(body, null, 2));
+    // writing addon_manifest.json
+    cli.action.start(`Updating ${color.blue('addon_manifest.json')}`);
+    writeFileSync('addon_manifest.json', JSON.stringify(body, null, 2));
     cli.action.stop();
   }
 }
