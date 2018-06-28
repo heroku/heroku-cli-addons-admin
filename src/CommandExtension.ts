5ce63402801939780e96190bcba013d8c86820c3
Aman Ibrahim <amanmibra@gmail.com>
2018-06-27 17:07:22 -0700

    Added axios library to command extension

--- src/CommandExtension.ts
+++ src/CommandExtension.ts
@@ -1,11 +1,20 @@
-import {Command} from '@heroku-cli/command'
+import {Command} from '@heroku-cli/command';
 import * as qqjs from 'qqjs';
+import axios from 'axios';
 
 export default abstract class CommandExtension extends Command {
   _qq:any
+  _axios:any
+
   get qq() {
     if (this._qq) return this._qq;
     this._qq = qqjs;
     return this._qq;
+  }
+
+  get axios() {
+    if (this._axios) return this._axios;
+    this._axios = axios;
+    return this._axios;
   }
 }
