edc824beffac77badc87d857bb86132b0137e3e4
Aman Ibrahim <amanmibra@gmail.com>
2018-06-29 08:27:07 -0700

    Replaced instances of this.heroku HTTPClient with this.axios

--- src/CommandExtension.ts
+++ src/CommandExtension.ts
@@ -14,7 +14,15 @@ export default abstract class CommandExtension extends Command {
 
   get axios() {
     if (this._axios) return this._axios;
-    this._axios = axios;
+    this._axios = axios.create({
+      baseURL: 'https://api.heroku.com',
+      headers: {
+        'Authorization': `Bearer ${process.env.HEROKU_API_KEY}`,
+        'content-type': 'application/json',
+        Accept: 'application/vnd.heroku+json; version=3',
+        version: 3,
+      }
+    });
     return this._axios;
   }
 }
--- src/commands/addons/admin/manifest/generate.ts
+++ src/commands/addons/admin/manifest/generate.ts
@@ -35,10 +35,19 @@ The file has been saved!`, ];
 
   async run() {
     const {flags} = this.parse(Generate);
-    const {body: account} = await this.heroku.get<Heroku.Account>('/account', {retryAuth: false});
+
+    // getting Heroku user data
+    let email: string | undefined = undefined;
+    await this.axios.get('/account')
+    .then ((res: any) => {
+      email = res.data.email;
+    })
+    .catch((err:any) => {
+      if (err) this.error(err)
+    });
 
     // checks if user is logged in, in case default user checking measures do not work
-    if (!account) {
+    if (!email) {
       this.error(color.red('Please login with Heroku credentials using `heroku login`.'));
     }
 
