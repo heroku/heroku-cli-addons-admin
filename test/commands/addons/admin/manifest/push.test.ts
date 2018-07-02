aaef1bde3fad44ac9648ad0b7834f00ff0a700e5
Aman Ibrahim <amanmibra@gmail.com>
2018-07-02 16:29:57 -0700

    Wrote tests for push

--- test/commands/addons/admin/manifest/push.test.ts
+++ test/commands/addons/admin/manifest/push.test.ts
@@ -1,18 +1,50 @@
 /* tslint:disable */
-// import {expect, test} from '@oclif/test'
-//
-// describe('addons:admin:manifest:push', () => {
-//   test
-//   .stdout()
-//   .command(['addons:admin:manifest:push'])
-//   .it('runs hello', ctx => {
-//     expect(ctx.stdout).to.contain('hello world')
-//   })
-//
-//   test
-//   .stdout()
-//   .command(['addons:admin:manifest:push', '--name', 'jeff'])
-//   .it('runs hello --name jeff', ctx => {
-//     expect(ctx.stdout).to.contain('hello jeff')
-//   })
-// })
+import {expect, test} from '@oclif/test';
+
+import { writeFileSync } from 'fs';
+
+// test manifest
+const manifest = require('./../../../../fixture/addon_manifest');
+
+// host for API isolation test
+const host = process.env.HEROKU_ADDONS_HOST || 'https://addons.heroku.com';
+
+describe('addons:admin:manifest:push', () => {
+  const pushTest = test
+  .nock('https://api.heroku.com', (api: any) => api
+    .get('/account')
+    .reply(200, {email: 'aman.ibrahim@heroku.com'})
+  )
+  .nock(host, (api: any) => {
+    api.post('/provider/addons', manifest)
+    .reply(200, manifest)
+  });
+
+  pushTest
+  .stdout()
+  .command(['addons:admin:manifest:push'])
+  .it('stdout contains manifest elements', (ctx:any) => {
+   Object.keys(manifest).forEach(key => {
+     if(key !== 'api') expect(ctx.stdout).to.contain(manifest[key])
+   })
+  });
+
+
+  // test
+  // .do(() => {  writeFileSync('addon_manifest.json', '') })
+  // .nock('https://api.heroku.com', (api: any) => api
+  //   .get('/account')
+  //   .reply(200, {email: 'aman.ibrahim@heroku.com'})
+  // )
+  // .nock(host, (api: any) => {
+  //   api.post('/provider/addons', '')
+  //   .replyWithError(400)
+  // })
+  // .stdout()
+  // .stderr()
+  // .command(['addons:admin:manifest:push'])
+  // .catch((err: any) => {
+  //   expect(err).to.be.an('error');
+  // })
+  // .it('error testing')
+})
