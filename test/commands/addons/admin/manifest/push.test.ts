e48a7ecfdce48dd274389aabb9def1e502272d93
Aman Ibrahim <amanmibra@gmail.com>
2018-07-03 15:45:45 -0700

    Error tests for push

--- test/commands/addons/admin/manifest/push.test.ts
+++ test/commands/addons/admin/manifest/push.test.ts
@@ -1,20 +1,16 @@
 /* tslint:disable */
-import {expect, test} from '@oclif/test';
+import {expect} from '@oclif/test';
+import test from '../../../../utils/test';
 
 import { writeFileSync } from 'fs';
 
 // test manifest
 const manifest = require('./../../../../fixture/addon_manifest');
-
 // host for API isolation test
 const host = process.env.HEROKU_ADDONS_HOST || 'https://addons.heroku.com';
 
 describe('addons:admin:manifest:push', () => {
   const pushTest = test
-  .nock('https://api.heroku.com', (api: any) => api
-    .get('/account')
-    .reply(200, {email: 'aman.ibrahim@heroku.com'})
-  )
   .nock(host, (api: any) => {
     api.post('/provider/addons', manifest)
     .reply(200, manifest)
@@ -29,22 +25,16 @@ describe('addons:admin:manifest:push', () => {
    })
   });
 
-
-  // test
-  // .do(() => {  writeFileSync('addon_manifest.json', '') })
-  // .nock('https://api.heroku.com', (api: any) => api
-  //   .get('/account')
-  //   .reply(200, {email: 'aman.ibrahim@heroku.com'})
-  // )
-  // .nock(host, (api: any) => {
-  //   api.post('/provider/addons', '')
-  //   .replyWithError(400)
-  // })
-  // .stdout()
-  // .stderr()
-  // .command(['addons:admin:manifest:push'])
-  // .catch((err: any) => {
-  //   expect(err).to.be.an('error');
-  // })
-  // .it('error testing')
+  test
+  .nock(host, (api: any) => {
+    api.post('/provider/addons', manifest)
+    .replyWithError(400)
+  })
+  .stdout()
+  .stderr()
+  .command(['addons:admin:manifest:push'])
+  .catch((err: any) => {
+    expect(err).to.be.an('error');
+  })
+  .it('error testing')
 })
