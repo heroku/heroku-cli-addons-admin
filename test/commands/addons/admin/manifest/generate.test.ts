6ca075ee751576bd7bb930a1b6b43cba1b843017
Aman Ibrahim <amanmibra@gmail.com>
2018-07-05 09:42:25 -0700

    Commented out tests. Template for test when tests are finally written.

--- test/commands/addons/admin/manifest/generate.test.ts
+++ test/commands/addons/admin/manifest/generate.test.ts
@@ -1,16 +1,19 @@
 /* tslint:disable */
-import {expect, test} from '@oclif/test'
-
-describe('addons:admin:manifest:generate', () => {
-  // test
-  // .nock('https://api.heroku.com', (api: any) => api
-  //   .get('/account')
-  //   .reply(200, {email: 'aman.ibrahim@heroku.com'})
-  // )
-  // .command(['addons:admin:manifest:generate', '-s', 'testing-123', '-a', 'test'])
-  // .do(() => {
-  //   console.log('')
-  // })
-  // .it('generates manifest')
-
-})
+// import {expect} from '@oclif/test'
+//
+// import test from '../../../../utils/test';
+//
+// describe('addons:admin:manifest:generate', () => {
+//   test
+//   .nock('https://api.heroku.com', (api: any) => api
+//     .get('/account')
+//     .reply(200, {email: 'aman.ibrahim@heroku.com'})
+//   )
+//   .stdout()
+//   .command(['addons:admin:manifest:generate', '-s', 'testing-123', '-a', 'test'])
+//   .do(() => {
+//     console.log('')
+//   })
+//   .it('generates manifest')
+//
+// })
