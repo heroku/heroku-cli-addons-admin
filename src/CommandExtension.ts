8fc15bb52967b20caab08f11883a8886db0437c3
Aman Ibrahim <amanmibra@gmail.com>
2018-07-17 16:14:01 -0700

    Updated axios to use heroku.auth rather than env variables

--- src/CommandExtension.ts
+++ src/CommandExtension.ts
@@ -3,6 +3,7 @@ import {Command} from '@heroku-cli/command'
 import * as qqjs from 'qqjs';
 
 import axiosInstance from './utils/axios';
+import axios from 'axios';
 
 export default abstract class CommandExtension extends Command {
   _qq:any
@@ -16,7 +17,16 @@ export default abstract class CommandExtension extends Command {
 
   get axios() {
     if (this._axios) return this._axios;
-    this._axios = axiosInstance;
+    this._axios = axios.create({
+      baseURL: 'https://api.heroku.com',
+      headers: {
+        'Authorization': `Bearer ${this.heroku.auth}`,
+        'content-type': 'application/json',
+        Accept: 'application/vnd.heroku+json; version=3',
+        version: 3,
+      }
+    });
     return this._axios;
   }
 }
+  
